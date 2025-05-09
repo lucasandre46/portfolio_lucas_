d364e3d78c5794d3bb40530f33f29bcf4a4d2b33
lucasandre46 <lucasandremiguel46@gmail.com>
2025-05-08 21:51:46 -0300

    atlz_08/05/25

--- Controller/banner.js
+++ Controller/banner.js
@@ -2,7 +2,28 @@ import { createHeader } from "./header.js";
 import { trocaMenu } from "./menu.js";
 
 window.trocaMenu = trocaMenu
+window.troca_bunner = troca_bunner
+
+
 
 document.addEventListener('DOMContentLoaded', ()=>{
     createHeader()
 });
+
+
+let banner_atual = 1;
+const qnt_banner = 3;
+
+export function troca_bunner(num_bunner){
+    let all_bunners = document.getElementById('all_bunners')
+    all_bunners.style.left = `-${num_bunner - 1}00vw`;
+     banner_atual = num_bunner;
+}
+
+setInterval(()=>{
+    if(banner_atual >= qnt_banner){
+        banner_atual = 0;
+    }
+    troca_bunner(banner_atual + 1);
+}, 5000);
+
--- Controller/menu.js
+++ Controller/menu.js
@@ -1,13 +1,14 @@
 export function trocaMenu(){
   let div_menu = document.getElementById('btn_conteiner');
-  let arrow = document.getElementById('flecha');
+  let btn_menu = document.getElementById('btn_menu');
+  
 
   if(div_menu.style.left == '0vw'){
     div_menu.style.left = '-30vw'
-    
+    btn_menu.style.transform = 'rotate(0deg)'
   } else {
     div_menu.style.left = '0vw'
-    
+    btn_menu.style.transform = 'rotate(-180deg)'
   }
 
 }
