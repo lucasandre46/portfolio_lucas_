f07858a5e9098ee2c19a95979b0cf1699149b149
lucasandre46 <lucasandremiguel46@gmail.com>
2025-05-12 21:55:18 -0300

    atlz_cartoes_12/05/25

--- Controller/pesquisa.js
+++ Controller/pesquisa.js
@@ -1,5 +1,28 @@
 import { createHeader } from "./header.js";
+import {cardsBD} from "./cardBD.js";
 
 document.addEventListener('DOMContentLoaded', ()=>{
      createHeader()
 });
+
+const main = document.getElementById('main');
+
+let div_geral = document.createElement('div');
+div_geral.className = 'div_geral'
+
+for(let i = 0; i < cardsBD.length; i++){
+
+     let div_card = document.createElement('div');
+     div_card.className = 'div_card'
+
+     let img_card = document.createElement('img');
+     img_card.src = cardsBD[i].img
+
+     let txt_card = document.createElement('h1');
+      txt_card.textContent = cardsBD[i].nome
+
+     main.appendChild(div_geral);
+     div_geral.appendChild(div_card);
+     div_card.appendChild(img_card);
+     div_card.appendChild(txt_card);
+}
