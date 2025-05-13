e7d703a725e4c038a7c329c07e740d0dd0ad484d
Lucas A. Miguel <lucasandremiguel46@gmail.com>
2025-05-12 23:27:01 -0300

    atlz_fds_de_noite

--- Controller/cardBD.js
+++ Controller/cardBD.js
@@ -1,12 +1,21 @@
 export const cardsBD = [
       { img: 'https://cdn.mos.cms.futurecdn.net/YDTUwK3HSgQmUHbc3Xw72o-970-80.jpg', nome: 'Iron Maiden', link: '' },
+
   { img: 'https://th.bing.com/th/id/R.15185535dea66dce7d23c784fb106d23?rik=zQ9HCE6BLeJUAw&pid=ImgRaw&r=0', nome: 'Black Sabbath', link: '' },
+
   { img: 'https://th.bing.com/th/id/R.fb80e5715778eb4ee6609a757d6d9745?rik=dAmBRvXcEzLs1w&pid=ImgRaw&r=0', nome: 'Dio', link: '' },
+
   { img: 'https://th.bing.com/th/id/R.89b319a7e6e80061e4d8a60fdc84da84?rik=wOvbeKrbDOa3xA&pid=ImgRaw&r=0', nome: 'Queen', link: '' },
+
   { img: 'https://logodix.com/logo/68750.jpg', nome: 'Led Zeppelin', link: '' },
+  
   { img: 'https://th.bing.com/th/id/R.3c50f2c3f80b6bca189705c639fab484?rik=gE%2b80%2f78m5phlA&pid=ImgRaw&r=0', nome: 'Megadeth', link: '' },
+
   { img: 'https://th.bing.com/th/id/OIP.Nv56PskimoFtITJtUaS6VwHaFj?cb=iwc2&rs=1&pid=ImgDetMain', nome: 'Metallica', link: '' },
+
   { img: 'https://th.bing.com/th/id/OIP.auIupEU53UDqgPd2C5SflwHaGS?cb=iwc2&rs=1&pid=ImgDetMain', nome: 'Slayer', link: '' },
+
   { img: 'https://th.bing.com/th/id/R.70fe10bb7bf5d10d87e0d8428e8ef959?rik=I0TsPXqoYuqEAw&pid=ImgRaw&r=0', nome: 'Ozzy Osburn', link: '' },
+
   { img: 'https://logodix.com/logo/36473.jpg', nome: 'Pink Floyd', link: '' }   
 ];
